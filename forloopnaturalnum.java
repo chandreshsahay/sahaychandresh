23860f76cc1cbfd1f98ed8466e86959ae636c9a3
Chandresh Sahay <chandreshsahay08874@gmail.com>
2024-07-23 12:25:53 +0530

    for loop natural no

---
+++ forloopnaturalnum.java
@@ -0,0 +1,9 @@
+public class forloopnaturalnum {
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=1; i<= 10; i++)
+            System.out.println(i);
+
+    }
+    
+}
